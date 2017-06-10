f5c56543554cb6b97b91f11dcfa321c27652a976
Connie Cheng <connie.cheng52@gmail.com>
2017-06-10 17:43:06 -0400

    working schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,22 @@
-DROP TABLE IF EXISTS time_group;
+DROP TABLE IF EXISTS date_group CASCADE;
+DROP TABLE IF EXISTS groups CASCADE;
+DROP TABLE IF EXISTS tweet_bucket CASCADE;
+
 CREATE TABLE date_group(
   id SERIAL PRIMARY KEY,
   day VARCHAR (10) NOT NULL,
-  hour VARCHAR (8) NOT NULL,
+  hour VARCHAR (8) NOT NULL
 );
 
-DROP TABLE IF EXISTS tweet_bucket;
-CREATE TABLE tweets(
-  id SERIAL PRIMARY KEY,
-  group_reference INTEGER REFERENCES group(id),
-  score INTEGER NOT NULL,
-  tweet_frequency INTEGER NOT NULL,
-  tweet_interval INTEGER REFERENCES date_group(id)
-);
-
-DROP TABLE IF EXISTS group;
-CREATE TABLE group(
+CREATE TABLE groups(
   id SERIAL PRIMARY KEY,
   group_name VARCHAR (20) NOT NULL
+);
+
+CREATE TABLE tweet_bucket(
+  id SERIAL PRIMARY KEY,
+  score INTEGER NOT NULL,
+  tweet_frequency INTEGER NOT NULL,
+  group_reference INTEGER REFERENCES groups(id),
+  tweet_interval INTEGER REFERENCES date_group(id)
 );
